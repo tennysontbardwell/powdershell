83c5d71f2f9d35d7371a6c8d012dbb4e3dbed64c
Sitar Harel <sitar@sitarharel.com>
2016-11-09 13:25:29 -0500

    fixed some formatting

--- model.mli
+++ model.mli
@@ -8,33 +8,30 @@
   
   type grid_t = (location_t * particle) Hashtbl.t 
 
-  (*[indices_of_particle] returns a list of locations where this particle type
-  * is found*)
+  (* [indices_of_particle] returns a list of locations where this particle type
+  * is found *)
   val indices_of_particle : particle_t -> location_t list
 
-  (*[particle_at_index] returns type of particle found at this index*)
+  (* [particle_at_index] returns type of particle found at this index *)
   val particle_at_index : location_t -> particle_t 
 
   (* [to_list] returns a list representation of the 
    * display held in the hashtable of pixel locations. Format will be each that 
    * each list will hold all of the particles in that row. There will be a 
-   * list of particle lists to represent the entire grid. 
-   *)
+   * list of particle lists to represent the entire grid. *)
   val to_list : grid_t -> particle_t list list 
   
   (* [set_pixel] takes in the the current grid, location and desired particle at
-   * that location and outputs the new grid
-   *)
+   * that location and outputs the new grid *)
   val set_pixel : location_t -> particle_t -> grid_t -> grid_t 
   
-  (*[get_grid_size] takes in the hashtable representing the grid and outputs
-   * a tuple of (width, height)
-   *)
+  (* [get_grid_size] takes in the hashtable representing the grid and outputs
+   * a tuple of (width, height) *)
   val get_grid_size : grid_t -> int * int
   
-  (*[change_grid_size] takes in the (width, height) of the desired grid and
+  (* [change_grid_size] takes in the (width, height) of the desired grid and
    * the current grid and outputs the new grid with the allocations of the old
-   * grid copied over.*)
+   * grid copied over. *)
   val change_grid_size : int * int -> grid_t -> grid_t 
   
   (* [empty_grid] takes in a desire grid size in the form (width, height) 
