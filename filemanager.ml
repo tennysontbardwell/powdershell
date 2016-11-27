d1c7b0824c504da23d642d2ac6ffc1b195afcd4f
Quinn Halpin <quinn.halpin@gmail.com>
2016-11-26 21:23:46 -0500

    added ability to write grid to json file and read from json file to the grid

--- filemanager.ml
+++ filemanager.ml
@@ -41,7 +41,47 @@ let read_rules path =
   let elements = j |> member "elements" |> to_list in
   elements |> List.map parse_elm
 
-(*write state which is type array array to json list list*)
-let read_state j  = failwith "unimplemented" 
-let write_state _ = failwith "unimplemented"
+(*[handle_loc] helper function for reading location from a json file*)
+let handle_loc (j: Basic.json list) = match j with
+  | [`Int a; `Int b] -> (a,b)
+  | _ -> failwith "error"
 
+(*[handle_loc] helper function for reading a string from a json file*)
+let handle_str (j: Basic.json list) = match j with 
+  | [`String a] -> a
+  | _ -> failwith "error"
+
+(*[handle_loc] helper function for reading a list from a json file*)
+let handle_lst (j: (color_t * Basic.json) list) = match j with
+  | [("loc", `List loc_lst); ("name", `List name_lst); ("color", `List color_lst)] -> 
+    (handle_loc loc_lst, {name = (handle_str name_lst); color = (handle_str color_lst)})
+  | _ -> failwith "bad json list"
+
+(*[rd_col] helper function for read_state to take in a json column and parse it*)
+let rd_col (j:Basic.json list) : (location_t*particle_t) list = 
+  let json_list = (j |> filter_assoc) in 
+  List.fold_left (fun acc x -> acc@[handle_lst x]) [] json_list
+
+(*[read_state] reads a json file from a path and outputs a grid*)
+let read_state (path:file_path_t) : Model.ArrayModel.grid_t = 
+  let j = Yojson.Basic.from_file path in
+  let json_lst_lst = j |> to_list |> filter_member "row" |> filter_list in
+  let json_lst_arr = Array.of_list (List.fold_left 
+      (fun acc col -> acc @ [rd_col col]) [] json_lst_lst) in
+  Array.fold_left (fun acc_row row -> 
+      Array.append acc_row [|(Array.of_list row)|] ) [||] json_lst_arr
+
+(*[wr_row] converts a row of the grid into json*)
+let wr_row (arr:((int*int)* particle_t) array) : Basic.json = 
+  `Assoc [("row", `List ((Array.fold_left 
+    (fun acc x -> match x with
+      | ((int_1, int_2), particle) -> acc@[(`Assoc
+            [("loc", `List [`Int int_1; `Int int_2]); 
+             ("name", `List [`String particle.name]);
+             ("color", `List [`String particle.color])]
+      )]) [] arr) @ [`Null]))]
+
+(*[write_state] writes a grid into a file and places file at "grid.json"*)
+let write_state grid:(ArrayModel.grid_t) : file_path_t=  
+  (`List (Array.fold_left (fun acc r -> acc@[(wr_row r)]) [] grid) )
+  |> to_file "grid.json"
