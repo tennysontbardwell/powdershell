9033d0a1df9e9bb7c65be46f1d6fb71240c8501f
Tennyson Taylor Bardwell <tennysontaylorbardwell@gmail.com>
2016-11-28 12:52:05 -0500

    shuffle implemented & optimization

--- helpers.ml
+++ helpers.ml
@@ -23,3 +23,21 @@ let rec transpose list = match list with
 (* applies f to x a total of i times. i >= 0 *)
 let rec foldi i f x = if i==0 then x else foldi (i-1) f (f x)
 
+(* https://stackoverflow.com/questions/15095541/how-to-shuffle-list-in-on-in-ocaml *)
+(* randomizes a list *)
+(* let shuffle d = *)
+(*     let nd = List.map (fun c -> (Random.bits (), c)) d in *)
+(*     let sond = List.sort compare nd in *)
+(*     List.map snd sond *)
+
+(* https://caml.inria.fr/pub/docs/manual-ocaml/libref/Array.html *)
+let swap a i j =
+  let t = a.(i) in
+  a.(i) <- a.(j);
+  a.(j) <- t
+
+(* https://caml.inria.fr/pub/docs/manual-ocaml/libref/Array.html *)
+let shuffle_array a =
+  Array.iteri (fun i _ -> swap a i (Random.int (i+1))) a; a
+
+let shuffle l = l |> Array.of_list |> shuffle_array |> Array.to_list
--- updater.ml
+++ updater.ml
@@ -18,41 +18,64 @@ let deoptionalize l =
     List.concat  @@ List.map (function | None -> [] | Some x -> [x]) l
 
 let rec receive_input inp g = match inp with (* SITAR WROTE THIS PLEASE FIX PLEASE *)
-| ((ElemAdd i)::t) -> ArrayModel.set_pixel i.loc (Some {name=i.elem; display=("a", (100, 100, 100))}) g; receive_input t g
+| ((ElemAdd i)::t) -> ArrayModel.set_pixel i.loc
+  (Some {name=i.elem; display=("a", (100, 100, 100))}) g |> ignore;
+  receive_input t g
 | _ -> g
 
 (* this is [start] after moving 1 in direction [dir] *)
 let transform start dir = (fst start + (fst dir), snd start + (snd dir))
 
+let can_apply_move grid move = match move with
+| Move_exc (name, start, final, _) ->
+  if ArrayModel.in_grid grid final then
+    match (ArrayModel.particle_at_index grid start,
+            ArrayModel.particle_at_index grid final) with
+    | (Some {name=p_name; display=display}, None) when p_name=name -> true
+    | _ -> false
+  else false
+| Change_exc (location, inital, final, _) ->
+  match ArrayModel.particle_at_index grid location with
+  | Some {name=p_name; display=_} when p_name=inital ->
+      true
+  | _ -> false
+
+
 (* this is all the leagal [Move_exc]s for a particular location on any grid *)
-let get_movements elm_rules loc name = elm_rules.movements
+let get_movements elm_rules loc name grid = elm_rules.movements
   |> List.map (fun (directions, probability) ->
     directions
     |> List.map (fun dir ->
       Move_exc (name, loc, (transform loc dir), probability)))
   |> List.fold_left (@) []
+  |> List.filter (can_apply_move grid)
+
+(* this is all legal [Change_exc]s for a particular location on any grid *)
+let get_changes elm_rules (x,y) name grid =
+  let neighbors =
+    [x+1,y+1; x+1,y; x+1,y-1; x,y-1; x-1,y-1; x-1, y; x-1,y+1; x,y+1] in
+  (* TODO this doesn't allow multiple changes or something *)
+  let get_interaction elm =
+    List.filter (fun (Change (x,_,_)) -> x=elm) elm_rules.interactions
+    |> (fun x -> match x with | h::t -> Some h | [] -> None) in
+  let get_space_interaction (x,y) =
+    ArrayModel.particle_at_index grid (x,y)
+    >>= (fun x -> x.name |> get_interaction)
+    >>= (fun (Change (f,t,p)) -> Change_exc ((x,y), f, t, p) |> return) in
+  let interactions = neighbors
+    |> List.map return
+    |> List.map (fun x -> bind x get_space_interaction)
+    |> deoptionalize in
+  interactions
+  |> List.filter (can_apply_move grid)
 
 (* this is a list of [move_t]s for a particular location on the grid *)
 let move_options rules grid (x,y) =
   match ArrayModel.particle_at_index grid (x,y) with
   | Some particle -> begin
     let elm_rules = List.assoc particle.name rules in
-    let neighbors =
-      [x+1,y+1; x+1,y; x+1,y-1; x,y-1; x-1,y-1; x-1, y; x-1,y+1; x,y+1] in
-    let get_interaction elm =
-      List.filter (fun (Change (x,_,_)) -> x=elm) elm_rules.interactions
-      |> (fun x -> match x with | h::t -> Some h | [] -> None) in
-    let get_space_interaction (x,y) =
-      try
-        ArrayModel.particle_at_index grid (x,y)
-        >>= (fun x -> x.name |> get_interaction)
-        >>= (fun (Change (f,t,p)) -> Change_exc ((x,y), f, t, p) |> return)
-      with Invalid_argument _ -> None in
-    let interactions = neighbors
-      |> List.map return
-      |> List.map (fun x -> bind x get_space_interaction)
-      |> deoptionalize in
-    let moves = get_movements elm_rules (x,y) particle.name in
+    let interactions = get_changes elm_rules (x,y) particle.name grid in
+    let moves = get_movements elm_rules (x,y) particle.name grid in
     interactions @ moves
   end
   | None -> []
@@ -63,28 +86,27 @@ let filter_moves (moves : move_t list) : move_t list =
     | Move_exc (_,_,_,p) | Change_exc (_,_,_,p) -> prob_filter item p in
   moves |> List.map filter |> deoptionalize
 
-let apply_moves grid moves =
-  let apply grid move = begin
+let apply grid move =
+  if can_apply_move grid move then
     match move with
     | Move_exc (name, start, final, _) -> begin
-      if ArrayModel.in_grid grid final then
-        match (ArrayModel.particle_at_index grid start,
-                ArrayModel.particle_at_index grid final) with
-        | (Some {name=p_name; display=display}, None) when p_name=name ->
-          grid
-          |> ArrayModel.set_pixel start None
-          |> ArrayModel.set_pixel final (Some {name=name; display=display})
-        | _ -> grid
-      else grid
+      match ArrayModel.particle_at_index grid start with
+      | Some {name=p_name; display=display} ->
+        grid
+        |> ArrayModel.set_pixel start None
+        |> ArrayModel.set_pixel final (Some {name=name; display=display})
+      | _ -> failwith "Internal Error 20394"
     end
     | Change_exc (location, inital, final, _) -> begin
       match ArrayModel.particle_at_index grid location with
       | Some {name=p_name; display=_} when p_name=inital ->
         grid |> ArrayModel.set_pixel location (Some {name=final; display=("a", (100, 100, 100))})
-      | _ -> grid
+      | _ -> failwith "Internal Error 4093108"
     end
-  end in
-  List.fold_left apply grid moves
+  else grid
+
+let apply_moves grid moves =
+  moves |> shuffle |> List.fold_left apply grid
 
 let next_step rules grid =
   let x_max,y_max = ArrayModel.get_grid_size grid in
