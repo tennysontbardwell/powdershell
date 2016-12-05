1ba963be21663e97b785a0e215f5679643ae8a86
Sitar Harel <sitar@sitarharel.com>
2016-12-05 02:48:51 -0500

    added bolt

--- main.mli
+++ main.mli
@@ -5,5 +5,5 @@ open Rules
  * Once run is executed, run will be expected to constantly listen for user 
  * inputs and run the next state. Run will only stop if it the user types a 
  * key command to end the game or clicks the button quit on the window.*)
-(* val run : rules_t -> grid_t -> unit *)
+val run : rules_t -> unit
 
